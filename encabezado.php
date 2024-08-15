a38eb50596a12156fa52fe5bd8c78f26519d2b42
LYONSELVIN <selvinico101@gmail.com>
2024-08-15 12:05:10 -0600

    pruebas1

---
+++ encabezado.php
@@ -0,0 +1,54 @@
+<?php
+ ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        background: url('https://st.depositphotos.com/1468732/4735/i/450/depositphotos_47355317-stock-photo-futuristic-white-hexagon-room.jpg') no-repeat center center fixed;
+        background-size: cover;
+    }
+    
+
+        body {
+            padding-top: 70px;
+        }
+    </style>
+    <title>Control de notas</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-success fixed-top">
+        <a class="navbar-brand" ">COLEGIO PREUNIVERSITARIO DEL NORTE</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="mostrar_estudiantes.php">ESTUDIANTES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="notaas_estudiante.php">NOTAS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="mostrar_materias.php">CUMPLEAÑOS</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="mostrar_materias.php">MATERIAS</a>
+                </li>
+            </ul>
+        </div>
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="CERRARSEIONPENDIENTE">SOPORTE / AYUDA&nbsp;<i class="fa fa-hands-helping"></i></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <main class="container-fluid">
+    
